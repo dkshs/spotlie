5854f647f37a3cb3e8fe0605f7b028a048767f73
DKSHS <88355080+ShadowsS01@users.noreply.github.com>
2023-01-22 15:24:30 -0300

    backend: creating the models

--- backend/music/models.py
+++ backend/music/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class Singer(models.Model):
+    id = models.UUIDField(
+        default=uuid4(),
+        primary_key=True,
+        unique=True,
+        null=False,
+        blank=False,
+        editable=False,
+    )
+    name = models.CharField(max_length=30, unique=True, null=False, blank=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Music(models.Model):
+    id = models.UUIDField(
+        default=uuid4(),
+        primary_key=True,
+        unique=True,
+        null=False,
+        blank=False,
+        editable=False,
+    )
+    title = models.CharField(max_length=80, null=False, blank=False)
+    singers = models.ManyToManyField(Singer, related_name="singers")
+    cover = models.ImageField(upload_to="cover", blank=False)
+    audio = models.FileField(upload_to="audio", blank=False)
+
+    def __str__(self) -> str:
+        return self.title
