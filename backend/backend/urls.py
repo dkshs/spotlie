d23b8e275910f0569cf0c63c93402f138b6c7fca
DKSHS <88355080+ShadowsS01@users.noreply.github.com>
2023-01-22 15:17:44 -0300

    backend: configuring media files

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from .api import api
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", api.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
