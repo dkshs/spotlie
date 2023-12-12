2ccc87f9f61aed690d570519b456f3551ebf7d66
dkshs <88355080+dkshs@users.noreply.github.com>
2023-12-12 13:29:45 -0300

    update(backend): improving user model

--- backend/backend/users/models.py
+++ backend/backend/users/models.py
@@ -1,5 +1,4 @@
 import uuid
-from secrets import token_hex
 
 from django.db import models
 
@@ -11,11 +10,6 @@ class User(models.Model):
     email = models.EmailField(null=False, blank=False)
     image = models.URLField(default="https://img.clerk.com/preview.png")
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def save(self, *args, **kwargs):
-        if not self.external_id:
-            self.external_id = f"user_{token_hex(8)}"
-        return super().save(*args, **kwargs)
 
     def __str__(self):
         return self.username
