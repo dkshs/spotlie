ac65ed1956a314b059704d4810beb8bcedb19d59
dkshs <88355080+dkshs@users.noreply.github.com>
2023-12-14 19:10:24 -0300

    update(backend): removing django-debug-toolbar

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from django.views import defaults as default_views
 
 from .api import api
@@ -32,7 +32,3 @@ if settings.DEBUG:
         ),
         path("500/", default_views.server_error),
     ]
-    if "debug_toolbar" in settings.INSTALLED_APPS:
-        import debug_toolbar
-
-        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
