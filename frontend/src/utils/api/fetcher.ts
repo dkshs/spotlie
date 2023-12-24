76d29a35ec437b397d21c6db445253e26e5ac815
dkshs <88355080+dkshs@users.noreply.github.com>
2023-12-24 01:26:32 -0300

    fix(frontend): adding validation for requests with status 204

--- frontend/src/utils/api/fetcher.ts
+++ frontend/src/utils/api/fetcher.ts
@@ -12,7 +12,17 @@ export async function fetcher<T = unknown>(
   const url = new URL(`${env.NEXT_PUBLIC_BACKEND_API_URL}${path}`);
   if (opts?.searchParams)
     url.search = new URLSearchParams(opts.searchParams).toString();
-  if (!opts?.token) return (await fetch(url, opts)).json() as Awaited<T>;
+  if (!opts?.token) {
+    const res = await fetch(url, opts);
+    if (res.status === 204) {
+      return null as Awaited<T>;
+    }
+    return res.json() as Awaited<T>;
+  }
   const headers = { ...opts.headers, Authorization: `Bearer ${opts.token}` };
-  return (await fetch(url, { ...opts, headers })).json() as Awaited<T>;
+  const res = await fetch(url, { ...opts, headers });
+  if (res.status === 204) {
+    return null as Awaited<T>;
+  }
+  return res.json() as Awaited<T>;
 }
