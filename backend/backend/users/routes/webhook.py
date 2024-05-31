3cce07324646873bd9198c58424af2ae8f074047
dkshs <88355080+dkshs@users.noreply.github.com>
2024-05-30 22:53:53 -0300

    fix(backend): ignore ruff error on webhook

--- backend/backend/users/routes/webhook.py
+++ backend/backend/users/routes/webhook.py
@@ -21,7 +21,7 @@ router = Router()
 
 
 @router.post("/webhook", response={200: Response, 400: Response})
-def webhook(request: HttpRequest):  # noqa: C901, PLR0912
+def webhook(request: HttpRequest):  # noqa: C901
     headers = request.headers
     payload = request.body
 
